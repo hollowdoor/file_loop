729732bd968464471856552f90d7870e308c503f
hollowdoor <hollowdoor99@gmail.com>
2015-12-03 23:35:58 -0600

    Changed fs.stat to fs.lstat to assure direct file finding.
    Updated version.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = function(list, cwd){
                 }
                 var name = found[index];
                 return new Promise(function(resolve, reject){
-                    fs.stat(name, function(stats){
+                    fs.lstat(name, function(stats){
                         resolve({name: name, stats: stats});
                     });
 
